96cadfaf3bf94c9cd3affa20d096002a546f7c92
yk-codex <292484868+yk-codex@users.noreply.github.com>
2026-06-29 00:51:16 +0900

    Fix option preload type declaration

--- src/preload/option-api.d.ts
+++ src/preload/option-api.d.ts
@@ -1,7 +1,13 @@
-import type { OptionApi } from './option-api'
+interface OptionApi {
+  callMain(): Promise<string>
+  minimize(): void
+  close(): void
+}
 
 declare global {
   interface Window {
     optionApi: OptionApi
   }
 }
+
+export {}
