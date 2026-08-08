004a69fa6bce964a637375fcf03aa74efdf5dfef
kurimoto <17593348+yukikuri@users.noreply.github.com>
2026-08-08 21:34:49 +0900

    add kc-intake-sender request header Refs #41

--- src/main/orval/mutators/kc-intake.ts
+++ src/main/orval/mutators/kc-intake.ts
@@ -1,8 +1,16 @@
+import { app } from 'electron'
 import { Env } from "@common/env";
 
 let baseUrl = 'https://api.kanlog.info';
 if (Env.isTestMode) {
   baseUrl = 'http://test.api.kanlog.info';
+}
+
+const HeaderNameKcIntakeSender = 'Kc-Intake-Sender'
+const senderName = 'koubrowser'
+
+const kcIntakeSender = (): string => {
+  return `${senderName}/${app.getVersion()}`
 }
 
 export const fetchWithBaseUrl = async <T>(
@@ -14,7 +22,17 @@ export const fetchWithBaseUrl = async <T>(
   //   ...options.headers,
   //   'Prefer': 'code=500',
   // };
-  const res = await fetch(fullUrl, options);
+
+  // 要求ヘッダに送信元情報追加
+  const headers = new Headers(options.headers)
+  if (!headers.has(HeaderNameKcIntakeSender)) {
+    headers.set(HeaderNameKcIntakeSender, kcIntakeSender())
+  }
+
+  const res = await fetch(fullUrl, { 
+    ...options,
+    headers
+  });
 
   const body = [204, 205, 304].includes(res.status) ? null : await res.text();
   const data = body ? JSON.parse(body) : {};
