3f905b910508daab2765b77716831c5f22511228
kurimoto <17593348+yukikuri@users.noreply.github.com>
2026-08-30 01:10:13 +0900

    nocheck drop area is locked

--- src/renderer/src/components/DropAreas.vue
+++ src/renderer/src/components/DropAreas.vue
@@ -2,13 +2,10 @@
 import { KcsUtil } from '@common/kcs'
 import DropArea from '@renderer/components/DropArea.vue'
 import DropHistoryCell from '@renderer/components/DropHistoryCell.vue'
-import { svdata } from '@renderer/store/svdata'
 import LockImage from '@renderer/assets/img/lock.svg'
-import { Env } from '@common/env'
 import { computed, onMounted, onUnmounted, ref } from 'vue'
 import { Spot } from '@common/map'
 import { areaNames } from '@common/area_name'
-import { mapInfo as storeMapInfo } from '@renderer/store/mapinfo'
 
 const props = defineProps<{
   area_id: number
@@ -51,29 +48,6 @@ function areaNoText(index: number): string {
   return `${props.area_id}-${index + 1}`
 }
 
-function isAreaLocked(index: number): boolean {
-  if (!isEventMap.value) {
-    return false
-  }
-  if (0 === index) {
-    return false
-  }
-  if (Env.isDevelopment) {
-    return false
-  }
-
-  const id = props.area_id * 10 + areaNos.value[index]
-  const mapInfos = svdata.mapinfos.length ? svdata.mapinfos : storeMapInfo.api_map_info
-  console.log(areaNos.value)
-  console.log(
-    'area locked',
-    props.area_id,
-    areaNo,
-    mapInfos.some((el) => el.api_id === id)
-  )
-  return !mapInfos.some((el) => el.api_id === id)
-}
-
 function indicatorClick(event: Event): void {
   if ((event.target as HTMLElement).classList.contains('is-locked')) {
     event.stopPropagation()
@@ -81,9 +55,6 @@ function indicatorClick(event: Event): void {
   selected_spot.value = null
 }
 
-function lockClick(event: Event): void {
-  event.stopPropagation()
-}
 </script>
 
 <template>
@@ -107,9 +78,7 @@ function lockClick(event: Event): void {
         <span
           class="areas-indicator"
           @click="indicatorClick"
-          :class="{ 'is-locked': isAreaLocked(props.i) }"
         >
-          <LockImage v-if="isAreaLocked(props.i)" @click="lockClick" />
           {{ areaNoText(props.i) }}
         </span>
       </template>
